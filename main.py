f983dfb59cc53d9b3c3388488d7c75594768f49f
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-03-10 01:23:18 -0300

    [main.py] Now creating the collection.

--- main.py
+++ main.py
@@ -8,4 +8,6 @@ load_dotenv(find_dotenv())
 username = os.environ.get("ARANGO_USER")
 password = os.environ.get("ARANGO_PASSWORD")
 
-conn = Connection(username=username, password=password)
+conn                = Connection(username=username, password=password)
+db                  = conn.createDatabase(name="ufrj")
+flowchartCollection = db.createCollection(name="Flowchart")
