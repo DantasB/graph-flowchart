ab125cfaa47dae55ae866e74c7f17397a6941859
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-03-10 00:54:12 -0300

    Adding main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+
+from pyArango.connection import *
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+username = os.environ.get("ARANGO_USER")
+password = os.environ.get("ARANGO_PASSWORD")
+
+conn = Connection(username=username, password=password)
